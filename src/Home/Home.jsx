5ba0a1a4749f92f5461a644f28c7538da9b1ffd8
iftekar-23 <aiftekar798@gmail.com>
2025-11-10 18:46:34 +0600

    added tooltip in home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { FaInfoCircle } from "react-icons/fa";
 import Banner from "../Component/Banner";
-import BillCard from "../Component/BillCard";
 import HowItWorks from "../Component/HowItWorks";
 import WhyChooseUs from "../Component/WhyChooseUs";
 import CategorySection from "../Component/CategorySection";
 import RecentBills from "../Component/RecentBills";
 
 const Home = () => {
-
   const sectionVariants = {
     hidden: { opacity: 0, y: 40 },
     visible: { opacity: 1, y: 0 },
+  };
+
+  const tooltipVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
   };
 
   return (
@@ -22,8 +26,14 @@ const Home = () => {
       transition={{ staggerChildren: 0.3 }}
     >
       {/* Banner Section */}
-      <motion.div variants={sectionVariants} transition={{ duration: 0.8 }}>
-        <Banner />
+      <motion.div
+        variants={sectionVariants}
+        transition={{ duration: 0.8 }}
+        className="relative"
+      >
+        <div className="flex justify-between items-center mb-4">
+          <Banner />
+        </div>
       </motion.div>
 
       {/* Category Section */}
@@ -32,7 +42,14 @@ const Home = () => {
         variants={sectionVariants}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-        <CategorySection />
+        <motion.div
+          variants={tooltipVariants}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          data-tooltip-id="global-tooltip"
+          data-tooltip-content="Browse through all available bill categories"
+        >
+          <CategorySection />
+        </motion.div>
       </motion.section>
 
       {/* Recent Bills */}
@@ -40,13 +57,22 @@ const Home = () => {
         variants={sectionVariants}
         transition={{ duration: 0.8, delay: 0.3 }}
       >
-        <RecentBills />
+        <motion.div
+          variants={tooltipVariants}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          data-tooltip-id="global-tooltip"
+          data-tooltip-content="See your most recent bills here"
+        >
+          <RecentBills />
+        </motion.div>
       </motion.div>
 
       {/* How It Works */}
       <motion.div
         variants={sectionVariants}
         transition={{ duration: 0.8, delay: 0.4 }}
+        data-tooltip-id="global-tooltip"
+        data-tooltip-content="Understand how UBM System works"
       >
         <HowItWorks />
       </motion.div>
@@ -55,6 +81,8 @@ const Home = () => {
       <motion.div
         variants={sectionVariants}
         transition={{ duration: 0.8, delay: 0.5 }}
+        data-tooltip-id="global-tooltip"
+        data-tooltip-content="Discover why users love UBM System"
       >
         <WhyChooseUs />
       </motion.div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,25 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import AuthProvider from './context/AuthProvider.jsx'
 import router from './router/routes.jsx'
+import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <AuthProvider>
        <RouterProvider router={router}/>
+       <Tooltip
+        id="global-tooltip"
+        place="top"
+        style={{
+          backgroundColor: "#2563eb",
+          color: "#fff",
+          fontSize: "13px",
+          borderRadius: "6px",
+          padding: "6px 10px",
+        }}
+      />
   </AuthProvider>
   </StrictMode>,
 )
